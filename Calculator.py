64c17a8f813352de6e61b6a76d6a49934216121f
HTN <96134288+htn-dev@users.noreply.github.com>
2022-01-18 16:00:27 -0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -4,7 +4,9 @@
 def addition ():
     print("Addition")
     n = float(input("Enter the number: "))
-    t = 0 # Total number enter
+    t = 0 
+    
+    # Total number enter
     ans = 0
     while n != 0:
         ans = ans + n
