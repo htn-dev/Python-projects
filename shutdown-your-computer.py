20eb9bda38070af78be590e8415ea134434f044f
HTN <96134288+htn-dev@users.noreply.github.com>
2022-01-22 13:19:12 -0500

    Update shutdown-your-computer.py

--- shutdown-your-computer.py
+++ shutdown-your-computer.py
@@ -1,3 +1,5 @@
+# This program will shutdown your computer when executed. Make sure to save all your works before execute it
+
 import os
 
 shutdown = input("Do you want to shut down your computer? (Y/N) ")
