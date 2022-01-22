ba7d1e182ebb8247384a9fed523b37371ad7dda8
HTN <96134288+htn-dev@users.noreply.github.com>
2022-01-22 13:47:41 -0500

    Update shutdown-your-computer.py

--- shutdown-your-computer.py
+++ shutdown-your-computer.py
@@ -3,9 +3,9 @@
 
 import os
 
-shutdown = input("Do you want to shut down your computer? (Y/N) ")
+shutdown = input("Do you want to shut down your computer? (y/n) ")
 
-if shutdown == 'Y':
+if shutdown == 'y':
     os.system('shutdown /s /t 1')
 else:
     print('Shutdown is not requested.')
