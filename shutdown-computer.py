dc69e7e1096d131e01bb37f38087e32c07e120dd
HTN <96134288+htn-dev@users.noreply.github.com>
2022-02-09 12:20:17 -0500

    Update shutdown-computer.py

--- shutdown-computer.py
+++ shutdown-computer.py
@@ -1,5 +1,5 @@
 # A Python program to shutdown your computer
-# WARNING: Make sure to save all your works before execute this program
+# !!! WARNING !!! Make sure to save all your works before execute this program
 
 import os
 
