16efe140e30a966893f368e303f49a091e95336e
HTN <96134288+htn-dev@users.noreply.github.com>
2022-02-18 15:04:23 -0500

    Update text-to-audio.py

--- text-to-audio.py
+++ text-to-audio.py
@@ -1,6 +1,5 @@
 # install the module with the following command:
 # pip install pyttsx3
-
 import pyttsx3
 engine = pyttsx3.init()
 engine.say('A python program that change text to voice. Testing...')
