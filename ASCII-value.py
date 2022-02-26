9888fec77c03a725881267e984e48501f46caf5f
HTN <96134288+htn-dev@users.noreply.github.com>
2022-02-26 18:36:42 -0500

    Update ASCII-value.py

--- ASCII-value.py
+++ ASCII-value.py
@@ -4,6 +4,6 @@
 # In c we can assign different
 # characters of which we want ASCII value
 
-c = input('What is your character? ')
+c = input('Please enter your character? ')
 # print the ASCII value of assigned character in c
 print("The ASCII value of '" + c + "' is", ord(c))
