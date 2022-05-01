41aae0f08610736ed53be8e41002331ec7bf838e
HTN <96134288+htn-dev@users.noreply.github.com>
2022-04-30 21:27:08 -0400

    Update Triangle-area-calculator.py

--- Triangle-area-calculator.py
+++ Triangle-area-calculator.py
@@ -3,7 +3,6 @@ print('Please enter the values of the three sides of the triangle: ')
 a = float(input('Enter first side value: '))
 b = float(input('Enter second side value: '))
 c = float(input('Enter third side value: '))
-
 # calculate the semi-perimeter
 
 s = (a + b + c) / 2
