94e4346bfd889c189f8c0ec04dec2e851abc9de4
HTN <96134288+htn-dev@users.noreply.github.com>
2022-04-30 21:27:19 -0400

    Update Triangle-area-calculator.py

--- Triangle-area-calculator.py
+++ Triangle-area-calculator.py
@@ -6,6 +6,5 @@ c = float(input('Enter third side value: '))
 # calculate the semi-perimeter
 s = (a + b + c) / 2
 # calculate the area
-
 area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
 print('The area of the triangle is %0.2f' %area)
