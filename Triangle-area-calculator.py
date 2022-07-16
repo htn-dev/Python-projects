27f395b1e3e8e871e8f8d8136009eea2e5c64f83
HTN <96134288+htn-dev@users.noreply.github.com>
2022-07-16 15:08:12 -0400

    Update Triangle-area-calculator.py

--- Triangle-area-calculator.py
+++ Triangle-area-calculator.py
@@ -1,5 +1,5 @@
 print('Please enter the values of the three sides of the triangle: ')
-  
+   
 # Take inputs from the user
 a = float(input('Enter first side value: '))
 b = float(input('Enter second side value: '))
